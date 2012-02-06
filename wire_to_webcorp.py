376b2ecfde682cb487981cdd2f11177e4c306d85
Attila Zseder <zseder@gmail.com>
2012-02-06 15:36:52 +0100

    wire_to_webcorp.py added - script that converts data from wire-info-extract to format used in our   programs

---
+++ wire_to_webcorp.py
@@ -0,0 +1,20 @@
+import re
+import sys
+
+separator = re.compile("<!-- DOCID:([^ ]*) .*-->$")
+unique_sep = file("splitcode.h").read().split("\n")[0].split("\"")[1]
+
+def main():
+    prev_id = None
+    for line in sys.stdin:
+        m = separator.match(line)
+        if m is not None:
+            if prev_id is not None:
+                print "DOCEND {0} {1}".format(unique_sep, prev_id)
+            prev_id = m.group(1).strip()
+            print "DOCSTART {0} {1}".format(unique_sep, prev_id)
+        else:
+            sys.stdout.write(line)
+
+if __name__ == "__main__":
+    main()
