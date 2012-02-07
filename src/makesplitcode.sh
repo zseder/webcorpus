41e910afb09bf09bd8067880b9a42ee035321348
Attila Zseder <zseder@gmail.com>
2012-02-07 15:42:17 +0100

    makesplitcode.sh now creates .py as well (for later scripts) Makefile changed to both build and install ("bin" target)

--- src/makesplitcode.sh
+++ src/makesplitcode.sh
@@ -2,7 +2,6 @@
 # generate random splitcode for batch processing of HTML files
 splitcode="$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
 splitcodelen=$[ $(echo $splitcode | wc -c) - 1 ]
-echo "#define SPLITCODE \"$splitcode\""
-echo "#define SPLITCODELEN $splitcodelen"
-
-
+echo "#define SPLITCODE \"$splitcode\"" > $1
+echo "#define SPLITCODELEN $splitcodelen" >>$1
+echo "SPLITCODE = $splitcode" > $2
