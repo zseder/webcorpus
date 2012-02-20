44320b271198f01639066b2b5a4d8ee202bb4c57
Attila Zseder <zseder@gmail.com>
2012-02-20 22:04:51 +0100

    neardupfilter now uses reader.h for reading but still doesn't filter anything just counts simhash

--- src/neardupfilter.cc
+++ src/neardupfilter.cc
@@ -1,12 +1,7 @@
-#include <string>
-#include <map>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
 #include <sstream>
 #include <bitset>
+
+#include "reader.h"
 
 typedef unsigned long uint64_t;
 
@@ -14,76 +9,27 @@ extern "C" {
     uint64_t charikar_hash64_wide( const char *data, long data_length, int stoken_size);
 }
 
-#include "splitcode.h"
-
-#define MAX_STR_LEN			512
-
 using namespace std;
-
-
-enum
-{
-	INITIAL = 0,
-	CONTENT,
-        META
-};
-
-char header[MAX_STR_LEN];
-char footer[MAX_STR_LEN];
-
-typedef vector<string> Doc;
 
 int main(int argc, char **argv)
 {
-	int state = INITIAL;
-
-	char buf[BUFSIZ];;
-    stringstream doc;
-	
-	sprintf(header, "DOCSTART %s", SPLITCODE);
-	sprintf(footer, "DOCEND %s", SPLITCODE);
-
-    long id;
-    char myheader[MAX_STR_LEN];
-
-	// get lines until eof
-	while(fgets(buf, BUFSIZ, stdin))
+    Doc doc;
+	while(get_doc(stdin, &doc))
 	{
-		// jump over the metadata
-		if(state == INITIAL)
-		{
-			// if content comes
-			if(strncmp(buf, header, SPLITCODELEN + 9) == 0)
-			{
-				state = CONTENT;
-                id = atoi(buf+SPLITCODELEN +9);
-                strcpy(myheader, buf);
-                doc.str("");
-			}
-		}
-		// parse content
-		else
-		{
-			// check for the end of content
-			if(strncmp(buf, footer, SPLITCODELEN + 7) == 0)
-			{
-                char* cstr = new char [doc.str().size()+1];
-                strcpy (cstr, doc.str().c_str());
-                long len = strlen(cstr);
-                uint64_t simhash = charikar_hash64_wide(cstr, len, 3);
+        stringstream docstream;
+        unsigned int line_num;
+        for(line_num = 1; line_num != doc.size() - 1; line_num++)
+            docstream << doc[line_num];
+        string docstring = docstream.str();
+        uint64_t simhash = charikar_hash64_wide(docstring.c_str(), docstring.size(), 3);
 
-                cout << myheader;
-                cout << bitset<64>(simhash) << "\n";
-                cout << buf;
-
-			    state = INITIAL;
-                doc.str("");
-			}
-			else
-            {
-                doc << buf;
-            }
-		}
+        /*for ( size_t i=0; i!=doc.size(); ++i )
+            cout << doc[i];
+        cout << endl;*/
+        cout << doc[0];
+        cout << bitset<64>(simhash) << "\n";
+        cout << doc[doc.size()-1];
+        doc.clear();
 	}
 
 	return 0;
