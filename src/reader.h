fa3e57392785cc00011a8d4e9a3ef853d1eb8768
Attila Zseder <zseder@gmail.com>
2012-02-20 18:35:50 +0100

    reading is now in different .h file instead of boilerplating it

---
+++ src/reader.h
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+
+#include "splitcode.h"
+#define MAX_STR_LEN			512
+
+using namespace std;
+typedef vector<string> Doc;
+
+int get_doc(FILE* f, Doc* doc)
+{
+	char header[MAX_STR_LEN];
+	char footer[MAX_STR_LEN];
+	char buf[BUFSIZ];
+	
+	sprintf(header, "DOCSTART %s", SPLITCODE);
+	sprintf(footer, "DOCEND %s", SPLITCODE);
+
+	// get lines until eof
+	while(fgets(buf, BUFSIZ, f))
+	{
+        // if a header came, we except content from now on
+        if(strncmp(buf, header, SPLITCODELEN + 9) == 0)
+        {
+            if(doc->size() != 0)
+            {
+                cerr << "Calling get_doc with non-empty vector!";
+                exit(-1);
+            }
+            doc->push_back(buf);
+        }
+		// parse content
+        else if(strncmp(buf, footer, SPLITCODELEN + 7) == 0)
+        {
+            doc->push_back(buf);
+            return 1;
+        }
+        else
+        {
+            if (strlen(buf) >= BUFSIZ -1)
+                cerr << "Very long line with BUFSIZ=" << BUFSIZ << " skipped." <<endl;
+            doc->push_back(buf);
+        }
+	}
+	return -1;
+}
