e09d0bb30301dfe359d2f465b8ad9e3d86687ab9
Attila Zseder <zseder@gmail.com>
2012-02-28 17:37:20 +0100

    clean_encoding: - now separated into c and h - prints good documents or - tries to convert bad documents to good and prints them

--- src/clean_encoding.c
+++ src/clean_encoding.c
@@ -12,164 +12,12 @@
 #include <cstring>
 
 #include "reader.h"
+#include "clean_encoding.h"
 
-#define VALID_CONT(x) (unsigned char)x <= 191 && (unsigned char)x >= 128
 
-char get_next_utf8_char_len(const char* buf, unsigned max_left_bytes)
-{
-    char n = 0;
-    unsigned char b0 = (unsigned char)buf[0];
-    if (b0 < 128)
-        n = 1;
-    else if (b0 < 192)
-        // first byte should not be this
-        n = -1;
-    else if (2 <= max_left_bytes && b0 < 224 &&
-            VALID_CONT(buf[1]))
-        n = 2;
-    else if (3 <= max_left_bytes && b0 < 240 &&
-            VALID_CONT(buf[1]) &&
-            VALID_CONT(buf[2]))
-        n = 3;
-    else if (4 <= max_left_bytes && b0 < 248 &&
-            VALID_CONT(buf[1]) &&
-            VALID_CONT(buf[2]) &&
-            VALID_CONT(buf[3]))
-        n = 4;
-    else if (5 <= max_left_bytes && b0 < 252 &&
-            VALID_CONT(buf[1]) &&
-            VALID_CONT(buf[2]) &&
-            VALID_CONT(buf[3]) &&
-            VALID_CONT(buf[4]))
-        n = 5;
-    else if (6 <= max_left_bytes && b0 < 254 &&
-            VALID_CONT(buf[1]) &&
-            VALID_CONT(buf[2]) &&
-            VALID_CONT(buf[3]) &&
-            VALID_CONT(buf[4]) &&
-            VALID_CONT(buf[5]))
-        n = 6;
-    return n;
-}
-
-#define NUM_ENCODINGS 4
-
-const char* possible_encodings[NUM_ENCODINGS] = { "ISO-8859-1", "ISO-8859-15", "windows-1250", "windows-1252" };
 iconv_t iconvs[NUM_ENCODINGS];
 
-void fix_encoding(const char* input)
-{
-    int scores[NUM_ENCODINGS];
-    int i;
-    for (i=0; i < NUM_ENCODINGS; i++)
-        scores[i] = 0;
-
-    int l = strlen(input);
-    for(i=0; i < l;)
-    {
-        size_t first = get_next_utf8_char_len(&input[i], (unsigned char)(l - i));
-        if (first == 0) 
-        {
-            // usually end of input
-            break;
-        }
-        if (first > 1 && first + i < l)
-        {
-            size_t second = get_next_utf8_char_len(&input[i+first], (unsigned char)(l - i - first));
-            if (second == 0) 
-            {
-                // usually end of input
-                break;
-            }
-            if (second > 1)
-            {
-                int local_scores[NUM_ENCODINGS];
-                int iconv_i;
-                for (iconv_i=0; iconv_i < NUM_ENCODINGS; iconv_i++)
-                    local_scores[iconv_i] = 0;
-                for (iconv_i = 0; iconv_i < NUM_ENCODINGS; iconv_i++)
-                {
-                    // initializing arguments for iconv
-                    //   for first character 
-                    char* first_in_buf = (char*) malloc((first)*sizeof(char));
-                    strncpy(first_in_buf, &input[i], first);
-                    char* first_in_ptr = first_in_buf;
-                    size_t first_l = first;
-                    char first_out_buf[1];
-                    char* first_out_ptr = first_out_buf;
-                    size_t first_ol = 1;
-
-                    //   for second character 
-                    char* second_in_buf = (char*) malloc(second*sizeof(char));
-                    strncpy(second_in_buf, &input[i+first], second);
-                    char* second_in_ptr = second_in_buf;
-                    size_t second_l = second;
-                    char second_out_buf[1];
-                    char* second_out_ptr = second_out_buf;
-                    size_t second_ol = 1;
-
-                    // calling iconv
-                    int res1 = iconv(iconvs[iconv_i], &first_in_ptr, &first_l, &first_out_ptr, &first_ol);
-                    if (first_ol == 1)
-                        continue;
-                    int res2 = iconv(iconvs[iconv_i], &second_in_ptr, &second_l, &second_out_ptr, &second_ol);
-                    if (second_ol == 1)
-                        continue;
-
-                    // saving iconv output
-                    unsigned char first_byte = (unsigned char) first_out_buf[0];
-                    unsigned char second_byte = (unsigned char) second_out_buf[0];
-
-                    // check if 2 result bytes could have been bytes of a utf-8 character
-                    if (first_byte >= 192 && first_byte < 223 &&
-                        second_byte >= 128 && second_byte < 191)
-                    {
-                        // creating arguments for second iconv 
-                        // to check which encoding of the possible ones are
-                        // most probably (based on frequencies
-                        char* back_in_buf = (char*) malloc(2*sizeof(char));
-                        strncpy(back_in_buf, &first_out_buf[0], (size_t)1);
-                        strncpy(back_in_buf+1, &second_out_buf[0], (size_t)1);
-                        char* back_in_ptr = back_in_buf;
-                        size_t back_in_l = 2;
-                        char back_out_buf[1];
-                        char* back_out_ptr = back_out_buf;
-                        size_t back_out_l = 1;
-
-                        // calling iconv
-                        if (iconv(iconvs[iconv_i], &back_in_ptr, &back_in_l, &back_out_ptr, &back_out_l) >= 0)
-                        {
-                            // TODO save a local score for actual character based on frequencies
-                            if (back_out_l < 1)
-                                printf("%d %s\n", iconv_i, back_in_buf);
-                                scores[iconv_i] += 1;
-                        }
-                        *back_in_ptr = 0; *back_out_ptr = 0; back_out_ptr = 0;
-                        free(back_in_buf);
-                    }
-                    first_in_ptr = 0; *first_out_ptr = 0; first_out_ptr = 0;
-                    second_in_ptr = 0; *second_out_ptr = 0; second_out_ptr = 0;
-                    free(first_in_buf);
-                    free(second_in_buf);
-                }
-
-            }
-            i += second;
-        }
-        else if (first == 1)
-            ;
-        else
-            ;
-        i += first;
-    }
-    for (int i=0; i < NUM_ENCODINGS; i++)
-    {
-        printf("%d\n", scores[i]);
-    }
-    printf("\n");
-}
-
-void add_freq(int* freqs, const char* input)
+void add_freq(long freqs[], const char* input)
 {
     int l = strlen(input);
     int i;
@@ -178,7 +26,7 @@ void add_freq(int* freqs, const char* input)
         char len = get_next_utf8_char_len(&input[i], (unsigned char)(l - i));
         if (len == 2)
         {
-            freqs[input[i]*256 + input[i+1]] += 1;
+            freqs[((unsigned char)input[i])*256 + ((unsigned char)input[i+1])] += 1;
         }
         if (len >= 1)
             i += len;
@@ -189,12 +37,17 @@ void add_freq(int* freqs, const char* input)
 
 int main(int argc, char **argv)
 {
-    int i_;
     Doc doc;
     // first create 2-byte utf8 statistics
-    int freq[65536];
-    for (i_=0; i_ < 65536; i_++)
-        freq[i_] = 0;
+    long freq[65536];
+    int i;
+    for (i=0; i < 65536; i++)
+        freq[i] = 0;
+
+    int scores[NUM_ENCODINGS];
+    for (i=0; i < NUM_ENCODINGS; i++)
+        scores[i] = 0;
+
     FILE* f = fopen(argv[1], "r");
 	while(get_doc(f, &doc) > 0)
 	{
@@ -202,24 +55,49 @@ int main(int argc, char **argv)
         for(line_num = 1; line_num != doc.size() - 1; line_num++)
         {
             const string& line = doc[line_num];
-            add_freq(freq, line.c_str());
+            add_freq(&freq[0], line.c_str());
         }
         doc.clear();
 	}
-    for (i_ = 0; i_ < 65536; i_++)
-    {
-        printf("%d %d\n", i_, freq[i_]);
-    }
-    
 
-    for (int i_=0; i_ < NUM_ENCODINGS; i_++)
-        iconvs[i_] = iconv_open(possible_encodings[i_], "utf-8");
+    for (i=0; i < NUM_ENCODINGS; i++)
+        iconvs[i] = iconv_open(possible_encodings[i], "utf-8");
 	while(get_doc(stdin, &doc) > 0)
 	{
         Doc::iterator it;
         for(it = doc.begin(); it != doc.end(); it++)
         {
-            fix_encoding(it->c_str());
+            fix_encoding((char*)it->c_str(), &freq[0], &scores[0], iconvs, -1, NULL);
+        }
+        int max = 0;
+        for (i=0; i < NUM_ENCODINGS; i++)
+            max = max > scores[i] ? max : scores[i];
+        for (i=0; i < NUM_ENCODINGS; i++)
+        {
+            if (max > 0 && scores[i] == max)
+            {
+                // do the replacing thing and exit then
+                // if tie, we choose first (they always give the same result)
+                printf(doc[0].c_str());
+
+                for(int line_num = 1; line_num<doc.size()-1; line_num++)
+                {
+                    char* result = (char*) calloc(doc[line_num].size(), sizeof(char));
+                    fix_encoding(doc[line_num].c_str(), &freq[0], &scores[0], iconvs, i, result);
+                    printf(result);
+                    free(result);
+                }
+
+                printf(doc[doc.size()-1].c_str());
+                break;
+            }
+            else if (max == 0)
+            {
+                for (int line_num = 0; line_num < doc.size(); line_num++)
+                    printf(doc[line_num].c_str());
+                break;
+
+            }
         }
         doc.clear();
 	}
---
+++ src/clean_encoding.h
@@ -0,0 +1,193 @@
+
+#define VALID_CONT(x) (unsigned char)x <= 191 && (unsigned char)x >= 128
+#define NUM_ENCODINGS 4
+
+const char* possible_encodings[NUM_ENCODINGS] = { "ISO-8859-1", "ISO-8859-15", "windows-1250", "windows-1252" };
+
+char get_next_utf8_char_len(const char* buf, unsigned max_left_bytes)
+{
+    char n = 0;
+    unsigned char b0 = (unsigned char)buf[0];
+    if (b0 < 128)
+        n = 1;
+    else if (b0 < 192)
+        // first byte should not be this
+        n = -1;
+    else if (2 <= max_left_bytes && b0 < 224 &&
+            VALID_CONT(buf[1]))
+        n = 2;
+    else if (3 <= max_left_bytes && b0 < 240 &&
+            VALID_CONT(buf[1]) &&
+            VALID_CONT(buf[2]))
+        n = 3;
+    else if (4 <= max_left_bytes && b0 < 248 &&
+            VALID_CONT(buf[1]) &&
+            VALID_CONT(buf[2]) &&
+            VALID_CONT(buf[3]))
+        n = 4;
+    else if (5 <= max_left_bytes && b0 < 252 &&
+            VALID_CONT(buf[1]) &&
+            VALID_CONT(buf[2]) &&
+            VALID_CONT(buf[3]) &&
+            VALID_CONT(buf[4]))
+        n = 5;
+    else if (6 <= max_left_bytes && b0 < 254 &&
+            VALID_CONT(buf[1]) &&
+            VALID_CONT(buf[2]) &&
+            VALID_CONT(buf[3]) &&
+            VALID_CONT(buf[4]) &&
+            VALID_CONT(buf[5]))
+        n = 6;
+    return n;
+}
+
+enum operations { COUNT, REPLACE };
+
+int count_or_replace_with_iconv(iconv_t myconv, const char* input, size_t first, size_t second, int operation, const long freq[], char* output)
+{
+    // initializing arguments for iconv
+    //   for first character 
+    char* first_in_buf = (char*) malloc((first)*sizeof(char));
+    strncpy(first_in_buf, &input[0], first);
+    char* first_in_ptr = first_in_buf;
+    char first_out_buf[1];
+    char* first_out_ptr = first_out_buf;
+    size_t first_l = first;
+    size_t first_ol = 1;
+
+    //   for second character 
+    char* second_in_buf = (char*) malloc(second*sizeof(char));
+    strncpy(second_in_buf, &input[first], second);
+    char* second_in_ptr = second_in_buf;
+    char second_out_buf[1];
+    char* second_out_ptr = second_out_buf;
+    size_t second_l = second;
+    size_t second_ol = 1;
+
+    // calling iconv
+    int res1 = iconv(myconv, &first_in_ptr, &first_l, &first_out_ptr, &first_ol);
+    if (first_ol == 1)
+        return 0;
+    int res2 = iconv(myconv, &second_in_ptr, &second_l, &second_out_ptr, &second_ol);
+    if (second_ol == 1)
+        return 0;
+
+    // saving iconv output
+    unsigned char first_byte = (unsigned char) first_out_buf[0];
+    unsigned char second_byte = (unsigned char) second_out_buf[0];
+
+    // check if 2 result bytes could have been bytes of a utf-8 character
+    if (first_byte >= 192 && first_byte < 223 &&
+        second_byte >= 128 && second_byte < 191)
+    {
+        if (operation == COUNT)
+        {
+            // creating arguments for second iconv 
+            // to check which encoding of the possible ones are
+            // most probably (based on frequencies
+            char* back_in_buf = (char*) malloc(2*sizeof(char));
+            strncpy(back_in_buf, &first_out_buf[0], (size_t)1);
+            strncpy(back_in_buf+1, &second_out_buf[0], (size_t)1);
+            char* back_in_ptr = back_in_buf;
+            size_t back_in_l = 2;
+            char back_out_buf[1];
+            char* back_out_ptr = back_out_buf;
+            size_t back_out_l = 1;
+
+            // calling iconv
+            if (iconv(myconv, &back_in_ptr, &back_in_l, &back_out_ptr, &back_out_l) >= 0)
+            {
+                if (back_out_l < 1)
+                {
+                    return freq[256 * first_byte + second_byte];
+                }
+            }
+            *back_in_ptr = 0; *back_out_ptr = 0; back_out_ptr = 0;
+            free(back_in_buf);
+        }
+        else if (operation == REPLACE)
+        {
+            output[0] = first_out_buf[0];
+            output[1] = second_out_buf[0];
+            return 1;
+        }
+    }
+    first_in_ptr = 0; *first_out_ptr = 0; first_out_ptr = 0;
+    second_in_ptr = 0; *second_out_ptr = 0; second_out_ptr = 0;
+    free(first_in_buf);
+    free(second_in_buf);
+}
+
+void fix_encoding(const char* input, const long freq[], int scores[], iconv_t iconvs[], int best_iconv, char* result)
+{
+    int i;
+    int l = strlen(input);
+    int pos_in_result = 0;
+    for(i=0; i < l;)
+    {
+        size_t first = get_next_utf8_char_len(&input[i], (unsigned char)(l - i));
+        if (first == 0) 
+        {
+            // usually end of input
+            break;
+        }
+        if (first > 1 && first + i < l)
+        {
+            size_t second = get_next_utf8_char_len(&input[i+first], (unsigned char)(l - i - first));
+            if (second == 0) 
+            {
+                // usually end of input
+                break;
+            }
+            if (second > 1)
+            {
+                if (best_iconv == -1)
+                {
+                    int local_scores[NUM_ENCODINGS];
+                    int iconv_i;
+                    for (iconv_i=0; iconv_i < NUM_ENCODINGS; iconv_i++)
+                        local_scores[iconv_i] = 0;
+                    for (iconv_i = 0; iconv_i < NUM_ENCODINGS; iconv_i++)
+                    {
+                        local_scores[iconv_i] += count_or_replace_with_iconv(iconvs[iconv_i], &input[i], first, second, COUNT, freq, NULL);
+                    }
+                    int max = 0;
+                    for (iconv_i=0; iconv_i < NUM_ENCODINGS; iconv_i++)
+                        max = max > local_scores[iconv_i] ? max : local_scores[iconv_i];
+                    for (iconv_i=0; iconv_i < NUM_ENCODINGS; iconv_i++)
+                        if (local_scores[iconv_i] == max)
+                            scores[iconv_i] += 1;
+                }
+                else
+                {
+                    char new_char[2];
+                    count_or_replace_with_iconv(iconvs[best_iconv], &input[i], first, second, REPLACE, NULL, &new_char[0]);
+                    strncpy(&result[pos_in_result], new_char, 2);
+                    pos_in_result += 2;
+                }
+            }
+            else
+            {
+                // second == 1 && first > 1
+                if (best_iconv != -1)
+                {
+                    strncpy(&result[pos_in_result], &input[i], first + second);
+                    pos_in_result += first + second;
+                }
+            }
+            i += second;
+        }
+        else if (first == 1)
+        {
+            // first == 1
+            if (best_iconv != -1)
+            {
+                strncpy(&result[pos_in_result], &input[i], 1);
+                pos_in_result += 1;
+            }
+        }
+        else
+            printf("error happened with value of first\n");
+        i += first;
+    }
+}
