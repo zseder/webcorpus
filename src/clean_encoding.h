07c6f17fc0260ad01a077f957311ce3e7ab950f0
Attila Zseder <zseder@gmail.com>
2012-03-07 11:11:18 +0100

    added windows-1251 to encodings (cyrillic!)

--- src/clean_encoding.h
+++ src/clean_encoding.h
@@ -1,8 +1,8 @@
 
 #define VALID_CONT(x) (unsigned char)x <= 191 && (unsigned char)x >= 128
-#define NUM_ENCODINGS 4
+#define NUM_ENCODINGS 5
 
-const char* possible_encodings[NUM_ENCODINGS] = { "ISO-8859-1", "ISO-8859-15", "windows-1250", "windows-1252" };
+const char* possible_encodings[NUM_ENCODINGS] = { "ISO-8859-1", "ISO-8859-15", "windows-1250", "windows-1251", "windows-1252" };
 
 char get_next_utf8_char_len(const char* buf, unsigned max_left_bytes)
 {
