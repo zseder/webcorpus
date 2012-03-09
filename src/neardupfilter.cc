4a584aafb11b3bc76356398c4025692b77e765a6
Attila Zseder <zseder@gmail.com>
2012-03-09 16:46:31 +0100

    bugfix in neardupfilter: now bits_in_16bits is initialized

--- src/neardupfilter.cc
+++ src/neardupfilter.cc
@@ -3,10 +3,11 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <stdint.h>
+#include <cstdlib>
 
 #include "reader.h"
 
-typedef unsigned long uint64_t;
 typedef map<int, uint64_t> IdSimhashMap;
 typedef map<uint64_t, vector<int> > SimhashIdMap;
 
@@ -18,16 +19,41 @@ extern "C" {
 #define MIN(a,b) a<b?a:b
 #define MAX(a,b) a>b?a:b
 #define ABS(a) a>=0?a:-a
+#define HAMMING_32(a,b) bit_count(a^b)
 
 using namespace std;
 
 IdSimhashMap g_id_to_simhash;
 SimhashIdMap g_simhash_to_id;
 
-static char bits_in_16bits [0x1u << 16] ;
+// variables and functions for fast bit counting:
+static char bits_in_16bits [0x1u << 16];
+int iterated_bitcount (unsigned int n)
+{
+    int count=0;
+    while (n)
+    {
+        count += n & 0x1u ;
+        n >>= 1 ;
+    }
+    return count ;
+}
+void compute_bits_in_16bits (void)
+{
+    unsigned int i ;
+    for (i = 0; i < (0x1u<<16); i++)
+        bits_in_16bits [i] = iterated_bitcount (i) ;
+    return ;
+}
+
+int bit_count (uint32_t n)
+{
+    return bits_in_16bits[n & 0xffffu] + bits_in_16bits[(n >> 16) & 0xffffu];
+}
+// end of bitcount section
+
 vector<int> search_duplicates_with_bitsum_bucketing()
 {
-    cerr << "Bitsum bucketing started.\n";
     vector<uint64_t> buckets[33][33];
     SimhashIdMap::iterator hash_it; 
     for (hash_it = g_simhash_to_id.begin(); hash_it != g_simhash_to_id.end(); ++hash_it)
@@ -36,7 +62,6 @@ vector<int> search_duplicates_with_bitsum_bucketing()
         int upper = bits_in_16bits[(hash_it->first >> 32) & 0xffffu] + bits_in_16bits[(hash_it->first >> 48) & 0xffffu];
         buckets[lower][upper].push_back(hash_it->first);
     }
-    cerr << "Bitsum bucketing ended.\n";
 
     vector<int> to_drop;
     int i = 0;
@@ -52,6 +77,7 @@ vector<int> search_duplicates_with_bitsum_bucketing()
         int lower = bits_in_16bits[hash_it->first & 0xffffu] + bits_in_16bits[(hash_it->first >> 16) & 0xffffu];
         int upper = bits_in_16bits[(hash_it->first >> 32) & 0xffffu] + bits_in_16bits[(hash_it->first >> 48) & 0xffffu];
         for (int possible_lower = MAX(0, lower - 3); possible_lower != (MIN(33, lower + 4)); possible_lower++)
+        {
             for (int possible_upper = MAX(0, upper - 3); possible_upper != (MIN(33, upper + 4)); possible_upper++)
             {
                 if (ABS(possible_lower - lower) + ABS(possible_upper - upper) > 3)
@@ -74,12 +100,14 @@ vector<int> search_duplicates_with_bitsum_bucketing()
                     }
                 }
             }
+        }
     }
     sort(to_drop.begin(), to_drop.end());
     vector<int>::iterator id_it = unique(to_drop.begin(), to_drop.end());
     to_drop.resize( id_it - to_drop.begin() );
     return to_drop;
 }
+
 
 void add_to_maps(int id, uint64_t hash)
 {
@@ -89,6 +117,8 @@ void add_to_maps(int id, uint64_t hash)
 
 int main(int argc, char **argv)
 {
+    // init of bitcount array
+    compute_bits_in_16bits();
     Doc* doc = new Doc();
     FILE* input = fopen(argv[1], "r");
 	while (get_doc(input, doc)>0)
