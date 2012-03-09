31f551abd35fae8496697ddaf3783d2fa3c95eaf
Attila Zseder <zseder@gmail.com>
2012-03-09 19:27:19 +0100

    modified:   dat/Makefile modified:   src/Makefile modified:   src/flex_utf8_chardef_replacer.py modified:   src/sentence_tokenizer.flex.template
    change: now sentence_tokenizer is built online before tokenizing,
    because when language is known, language-specific nonbreaking prefixes
    are inserted into flex code, and Makefiles modified to do this
    
    dat/Makefile other changes:
    - hunspell bound change
    - time added to most commands to measure it

--- src/flex_utf8_chardef_replacer.py
+++ src/flex_utf8_chardef_replacer.py
@@ -44,11 +44,25 @@ def to_flex(char_class_name, characters):
     result += char_class_name + " (" + "|".join("{" + char_class_name + "_" + str(i) + "}" for i in xrange(line_num+1)) + ")\n"
     return result
 
+nolangmsg = """WARNING: Creating sentence tokenizer without language specific 
+nonbreaking prefixes. Sentence tokenizer will be rebuilt online, 
+i.e. before tokenizing, where language is already known\n"""
+filenotfoundmsg = """WARNING: Creating sentence tokenizer without language specific 
+nonbreaking prefixes because file %s not found\n""" 
 def main():
     small_one, small_two, small_three = read_hex_bytes(file(sys.argv[1]))
     capital_one, capital_two, capital_three = read_hex_bytes(file(sys.argv[2]))
+    if(sys.argv[3].endswith("undefined.nbp")):
+        sys.stderr.write(nolangmsg)
+        nonbreaking_prefixes = None
+    else:
+        try:
+            nonbreaking_prefixes = [w.strip() for w in file(sys.argv[3]).read().split("\n") if len(w.strip()) > 0]
+        except IOError:
+            sys.stderr.write(filenotfoundmsg % sys.argv[3])
+            nonbreaking_prefixes = None
 
-    flex_file = file(sys.argv[3])
+    flex_file = file(sys.argv[4])
     flex_data = flex_file.read()
     s = ""
     s += to_flex("LATIN_SMALL_1_BYTE", small_one)
@@ -61,6 +75,11 @@ def main():
     s += "LATIN_CHARACTER_CAPITAL ({LATIN_CAPITAL_1_BYTE}|{LATIN_CAPITAL_2_BYTE}|{LATIN_CAPITAL_3_BYTE})\n"
     s += "LATIN_CHAR ({LATIN_CHARACTER_SMALL}|{LATIN_CHARACTER_CAPITAL})\n"
     flex_data = flex_data.replace("INSERT_CHARACTER_DEFS", s)
+    if nonbreaking_prefixes is not None:
+        flex_data = flex_data.replace("INSERT_LANGUAGE_NONBREAKING_PREFIX", "|\"" + "\"|\"".join(nonbreaking_prefixes) + "\"")
+    else:
+        flex_data = flex_data.replace("INSERT_LANGUAGE_NONBREAKING_PREFIX", "")
+
     sys.stdout.write(flex_data)
 
 if __name__ == "__main__":
