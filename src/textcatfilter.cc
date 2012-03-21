b6f14725f196d80146c30587ab3faed3c85831be
Attila Zseder <zseder@gmail.com>
2012-03-21 11:41:29 +0100

    LGPL header insterted to src files

--- src/textcatfilter.cc
+++ src/textcatfilter.cc
@@ -1,3 +1,25 @@
+/*
+Copyright 2011 Attila Zseder
+Email: zseder@gmail.com
+
+This file is part of webcorpus
+url: https://github.com/zseder/webcorpus
+
+webcorpus pipeline is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 /**
  * runs a textcat filter on documents based on language fiven in second
  * argument and config in first argument
