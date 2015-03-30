f1b8dd437aba56665b87d253d65c50cf35feae0f
Zseder Attila <zseder@bethe.phy.bme.hu>
2015-03-30 14:51:47 +0200

    modified:   webcrawl/settings.py some parameter changes

--- scrapy/webcrawl/webcrawl/settings.py
+++ scrapy/webcrawl/webcrawl/settings.py
@@ -16,11 +16,11 @@ FEED_FORMAT = 'xml'
 FEED_URI = 'dump.xml'
 #SPIDER_MIDDLEWARES = { 'webcrawl.ignore.IgnoreVisitedItems': 560 }
 
-CONCURRENT_REQUESTS = 50
+CONCURRENT_REQUESTS = 200
 LOG_LEVEL = 'INFO'
 COOKIES_ENABLED = False
 RETRY_ENABLED = False
-DOWNLOAD_TIMEOUT = 5
+DOWNLOAD_TIMEOUT = 15
 REDIRECT_ENABLED = False
 
 DEPTH_PRIORITY = 1
@@ -28,7 +28,7 @@ SCHEDULER_DISK_QUEUE = 'scrapy.squeue.PickleFifoDiskQueue'
 SCHEDULER_MEMORY_QUEUE = 'scrapy.squeue.FifoMemoryQueue'
 
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'webcrawl (+http://www.yourdomain.com)'
